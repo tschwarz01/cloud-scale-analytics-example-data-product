54fc524b621cf3595ec0b3741abf60cdbeeaad2f
Thomas Schwarz <thosch@microsoft.com>
2022-06-20 07:04:35 -0500

    updates azurerm dependency due to bug w/ azurerm_client_config

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.7.0"
+      version = "3.10.0"
     }
   }
   required_version = ">= 0.15"
